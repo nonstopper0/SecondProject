29820a13fb51b98615e87c0998d6758c325581e6
nonstopper0 <nathanielredmon@gmail.com>
2019-07-09 23:01:31 -0600

    MoneyDisplay

--- App.py
+++ App.py
@@ -12,16 +12,17 @@ black = [0, 0, 0]
 white = [255, 255, 255]
 grey = [55, 55, 55]
 running = True
+money = 0
 
 
 class Button:
-    def __init__(self, x, y, width, height, text, textSize, textcolor, boxcolor):
+    def __init__(self, x, y, width, height, text, textsize, textcolor, boxcolor):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.text = text
-        self.textSize = textSize
+        self.textsize = textsize
         self.textcolor = textcolor
         self.boxcolor = boxcolor
 
@@ -29,29 +30,34 @@ class Button:
         # Create Button outline
         rectangle = pygame.draw.rect(displayWindow, self.boxcolor, pygame.Rect(self.x, self.y, self.width, self.height))
         # Create Text Object
-        textfont = pygame.font.SysFont("Myriad Pro", self.textSize)
+        textfont = pygame.font.SysFont("Myriad Pro", self.textsize)
         textSurf = textfont.render(self.text, 2, self.textcolor)
 
         # get rect and width of text generated. Then divide by 2 to help center
-        textRect, textRectSub = textSurf.get_rect(),textSurf.get_rect().width / 2
+        textRect = textSurf.get_rect()
+        textRectWidth = textSurf.get_rect().width / 2
+        textRectHeight = textSurf.get_rect().height / 2
         # move text Rect to Rectangle.Center
         textRectmove = textRect.move(rectangle.center)
-        textFinal = (textRectmove[0] - textRectSub)
+        textMoveLeft = (textRectmove[0] - textRectWidth)
+        textMoveUp = (textRectmove[1] - textRectHeight)
         # create new text rect object using subtracted numbers
-        textNew = (textFinal, textRectmove[1], textRectmove[2], textRectmove[3])
+        textNew = (textMoveLeft, textMoveUp, textRectmove[2], textRectmove[3])
         # display text to screen
         displayWindow.blit(textSurf, textNew)
         # return rectangle rect for an easier collide method using collidepoint()
         return rectangle
 
 
-PlayButton = Button(500, 500, 200, 200, "Hey now", 30, grey, black)
+MoneyButton = Button(500, 500, 200, 200, "MONEY", 40, white, black)
+MoneyDisplay = Button(10, 10, 50, 50, str(money), 40, white, black)
 
 
 def visuals():
-    global Test
+    global MoneyB, MoneyD
     displayWindow.fill(white)
-    Test = PlayButton.draw()
+    MoneyB = MoneyButton.draw()
+    MoneyD = MoneyDisplay.draw()
     pygame.display.update()
     clock.tick(30)
 
@@ -64,8 +70,9 @@ while running:
         if event.type == pygame.KEYDOWN:
             pass
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if Test.collidepoint(mouse):
-                print("hey")
+            if MoneyB.collidepoint(mouse):
+                money += 1
+                print(money)
     visuals()
 
 print("Program Ending...")
